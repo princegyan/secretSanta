f62f3b137f004bbdae28baf89c444bb1147fa034
Dennis Effa Amponsah <kamponsah.amponsah@gmail.com>
2020-01-01 20:06:56 +0000

    skeleton done

--- GiftExchange/app.py
+++ GiftExchange/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request 
 import csv
 import sqlite3 as sql
 
@@ -13,6 +13,19 @@ def index():
     return render_template(
         "index.html", numbers=list_of_numbers, names=list_of_names
     )
+
+@app.route("/fetch", methods=["GET","POST"])
+def fetch():
+    if request.method == 'GET':
+        returned_number="Dennis"
+        if returned_number =="" or None:
+            return render_template("returnedname.html", name="No name")
+        else:
+            return render_template("returnedname.html", name="returned_number")
+
+    else:
+        number = request.form['number']
+        return render_template("returnedname.html", name=number)
 
 
 if __name__ == "__main__":
