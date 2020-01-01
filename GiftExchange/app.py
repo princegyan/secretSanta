19fceabbf463a736090482a93059bc957fcb6932
Dennis Effa Amponsah <kamponsah.amponsah@gmail.com>
2020-01-01 19:23:36 +0000

    dummy data init

--- GiftExchange/app.py
+++ GiftExchange/app.py
@@ -5,11 +5,13 @@ import sqlite3 as sql
 
 app = Flask(__name__)
 
+list_of_numbers=[1,2,3]
+list_of_names=["Dennis", "Ming", "Bernice"]
 
 @app.route("/")
 def index():
     return render_template(
-        "index.html"
+        "index.html", numbers=list_of_numbers, names=list_of_names
     )
 
 
